0b21826d22cf923c735e5a17a731634f02a68969
cqzongjian <cqzongjian@gmail.com>
2018-04-28 10:51:57 +0800

    模板

---
+++ src/views/_blank/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="app-container">
+
+
+  </div>
+</template>
+
+<script>
+import waves from '@/directive/waves'; // 水波纹指令
+
+export default {
+  name: 'menumgr',
+  directives: {
+    waves
+  },
+  data() {
+    return {
+
+    }
+  },
+  filters: {
+
+  },
+  created() {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+<style rel="stylesheet/scss" lang="scss" scoped>
+
+
+</style>
